3597fd46b6bb891dc84d9b86044804f51babffaa
Tabousnus <gustavomansilla88@gmail.com>
2022-09-02 19:41:24 -0300

    Login script ocultar

---
+++ js/Index.js
@@ -0,0 +1,12 @@
+function MostrarLogin() {
+    document.getElementById('Login').style.visibility = 'visible';
+    document.getElementById('BotonesLogin').style.visibility = 'hidden';
+}
+function MostrarRegister() {
+  document.getElementById('Register').style.visibility = 'visible';
+  document.getElementById('BotonesLogin').style.visibility = 'hidden';
+}
+function YaTengoCuenta() {
+    document.getElementById('Login').style.visibility = 'visible';
+    document.getElementById('Register').style.visibility = 'initial';
+}
